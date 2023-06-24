9bd25d1a2eb99f483bf5132a738f6caa726fee18
Ezra Yehuwalashet <35451409+ezraermy@users.noreply.github.com>
2023-06-24 11:57:35 +0300

    Create flowerbed.js

---
+++ flowerbed.js
@@ -0,0 +1,14 @@
+const canPlaceFlowers = (flowerbed, n){
+  let count = 0;
+  const length = flowerbed.length;
+
+  for (let i = 0; i < length && count; i++){
+    if (flowerbed[i] === 0){
+      const prev = (i === 0)?0:flowerbed[i-1]
+      const next = (i === length - 1)?0:flowerbed[i+1]
+      count++;
+      flowerbed[i] = 1;
+    }
+  }
+  count >= n;
+}
