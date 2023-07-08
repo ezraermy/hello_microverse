10524ad7a5d4da21889d3e04d071d94e18ae68fe
Ezra Yehuwalashet <35451409+ezraermy@users.noreply.github.com>
2023-07-08 12:56:39 +0300

    Create Symmetric-tree.rb

---
+++ Symmetric-tree.rb
@@ -0,0 +1,17 @@
+class Symmetry
+att-accessor :val, :left :right
+  def initialize (val=0, left=nil, right=nil)
+    @val = val
+    @left = left
+    @right = right
+  end
+  def is_symmerty(root)
+    return true if root.nil?
+  end
+  def is_mirror(left, right)
+    return true if left.nil && right.nil?
+    return false if left.nil || right.nil || left.val != right.val
+    left.val == right.val && is_mirror(left.left, right.right) && is_mirror(left.right, right.left)
+  end
+end
+    
