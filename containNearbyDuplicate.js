356bf51095ae4f3ec2485ca7fbe5097f4c6a1101
Ezra Yehuwalashet <35451409+ezraermy@users.noreply.github.com>
2023-07-11 20:24:52 +0300

    Update containNearbyDuplicate.js

--- containNearbyDuplicate.js
+++ containNearbyDuplicate.js
@@ -1,6 +1,7 @@
 const containNearbyDuplicate = (nums, k) => {
   const numIndices = {}
   for (let i = 0; i < i < nums.length; i++){
+    # go through each element of nums
     let num = numIndices[i];
     if (num in numIndices && i - numIndices[num] <= k){
       return true 
