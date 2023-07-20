897953a40947adabe708ea7c3d4545a5563b4f19
Ezra Yehuwalashet <35451409+ezraermy@users.noreply.github.com>
2023-07-20 22:01:38 +0300

    Update two_sums.rb

--- two_sums.rb
+++ two_sums.rb
@@ -1,15 +1,14 @@
-#The two_sum function takes an array of integers nums and an integer target as input. 
-#It iterates through the array, calculating the complement (the value needed to reach the target) 
-#for each element. If the complement exists in the hash map, 
-#it means we have found the two numbers that add up to the target. 
-#In that case, we return the indices of the two numbers. If no solution is found, an empty array is returned.
-
+#The two_sum function takes an array of integer nums and an integer target as input. 
 def two_sum(nums, target)
   num_map = {}
+  #It iterates through the array, calculating the complement (the value needed to reach the target) 
   for nums.each_with_index do |num, index|
-    complement = target - num
-    if num_map.include?(complement)
-      return [num_map[complement], index]
+    #for each element. If the complement exists in the hash map, 
+    #it means we have found the two numbers that add up to the target. 
+    compliment = target - num
+    if num_map.include?(compliment)
+      #In that case, we return the indices of the two numbers. If no solution is found, an empty array is returned.
+      return [num_map[compliment], index]
     end
     num_map[num] = index
   end
